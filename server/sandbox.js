0b8677190eb748d8990d8344bca674b0031937f6
Maélis YONES <maelis.yones@edu.devinci.fr>
2022-02-06 22:50:21 +0100

    scrape_3

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
+//const dedicatedbrand = require('./sources/dedicatedbrand');
 //const adresseparisbrand = require('./sources/adresseparisbrand');
-//const montlimartbrand = require('./sources/montlimartbrand');
+const montlimartbrand = require('./sources/montlimartbrand');
 
 // The link of the different brands
 // https://www.dedicatedbrand.com/en/men/news
 // https://adresse.paris/602-nouveautes
 // https://www.montlimart.com/toute-la-collection.html
 
-async function sandbox(eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox(eshop = 'https://www.montlimart.com/toute-la-collection.html') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    //const products = await dedicatedbrand.scrape(eshop);
     //const products = await adresseparisbrand.scrape(eshop);
-   // const products = await montlimartbrand.scrape(eshop);
+    const products = await montlimartbrand.scrape(eshop);
     console.log(products);
     console.log('done');
     process.exit(0);
